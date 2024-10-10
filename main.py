03b41988e93ca06e28ddbe1b9e490d112f30df1f
hieu2211 <43336147+hieu2211@users.noreply.github.com>
2024-10-10 03:22:19 +0000

    update code add front

--- main.py
+++ main.py
@@ -11,14 +11,6 @@ CORS(app)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        # Lấy dữ liệu cần thiết từ form hoặc từ API
-        # secret = "YOUR_SECRET_KEY"  # Thay bằng secret của bạn
-        # timestamp = str(int(time.time()))  # Lấy timestamp hiện tại
-        # body = "test-body"  # Chuỗi body mẫu hoặc thực tế
-
-        # # Tạo chữ ký
-        # signature = generate_signature(secret, timestamp, body)
-
         # Chuyển hướng qua trang v1
         return redirect(url_for('v1fbads'))
 
@@ -34,34 +26,9 @@ def home():
 def v1fbads():
     print(f"request {request}")
     if request.method == 'POST':
-        # try:
-        # Get form data
-        # if request.is_json:
+
         print(f"request {request}")
-        # APP_TOKEN = request.form['APP_TOKEN']
-        # PERSONAL_BASE_TOKEN = request.form['PERSONAL_BASE_TOKEN']
-        # TABLE_ID = request.form['TABLE_ID']
-        # access_token = request.form['access_token']
-        # list_account = request.form['list_account'].split(',')
 
-        # since = request.form['since']
-        # until = request.form['until']
-        # level_ads = request.form['level_ads']
-        # # Format dates to 'yyyy-mm-dd'
-        # since = pd.to_datetime(since).strftime('%Y-%m-%d')
-        # until = pd.to_datetime(until).strftime('%Y-%m-%d')
-
-        # # Save these values in the synsc_facebook_ads module
-        # from v1.controllers import synsc_facebook_ads
-        # synsc_facebook_ads.APP_TOKEN = APP_TOKEN
-        # synsc_facebook_ads.PERSONAL_BASE_TOKEN = PERSONAL_BASE_TOKEN
-        # synsc_facebook_ads.TABLE_ID = TABLE_ID
-        # synsc_facebook_ads.access_token = access_token
-        # synsc_facebook_ads.list_account = list_account
-        # synsc_facebook_ads.last_2day = since
-        # synsc_facebook_ads.yesterday = until
-        # synsc_facebook_ads.level_ads = level_ads
-        # # Call the main function
         data = request.get_json()
         print(data)
 
@@ -103,11 +70,6 @@ def v1fbads():
                 status='error',
                 message=
                 "Please double check the information you have configured.")
-    # except Exception as e:
-    #     # Log và trả về thông báo lỗi
-    #     error_message = str(e)
-    #     return jsonify(status='error',
-    #                    message=f"An error occurred: {error_message}")
 
     return render_template('v1-fbads.html')
 
